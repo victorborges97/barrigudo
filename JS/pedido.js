3e019e6f0908d98d9236b28b8b9fdcc710c93dea
João Victor Dutra de Oliveria Borges <borges.jvdo@gmail.com>
2020-05-30 17:17:14 -0300

    atualizaçao

--- JS/pedido.js
+++ JS/pedido.js
@@ -1,3 +1,12 @@
 function adicionarcarrinho(){
     return alert('EM CONSTRUÇÃO')
 }
+
+function precototalquantidade(){
+    valortamanhol = document.getElementById(valor-1).value
+    valort = parseFloat(valortamanhol)
+    
+    return console.log(valort)
+}
+
+console.log(precototalquantidade)
