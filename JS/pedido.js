a1fcf739e0b7d1c21b6f83ac105b374e8eec8d89
João Victor Dutra de Oliveria Borges <borges.jvdo@gmail.com>
2020-06-01 15:36:38 -0300

    swicht case adicionado as function

--- JS/pedido.js
+++ JS/pedido.js
@@ -1,80 +1,102 @@
-function adicionarcarrinho1(){
-    var nome = document.getElementById('button-name1').name;
-    var quantidade = document.getElementById('quantidadeprimeiro').value;
-    var valor = document.getElementById('tamanhoprimeiro').value;
+function adicionarcarrinho(id){
+    switch (id) {
+        case 'gordobacon':
+            console.log("Entrou no Gordobacon")
+            var nome = document.getElementById('gordobacon').name;
+            var quantidade = document.getElementById('quantidadeprimeiro').value;
+            var valor = document.getElementById('tamanhoprimeiro').value;
 
-    alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-    console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-}
+            alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+            console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
 
-function adicionarcarrinho2(){
-    var nome = document.getElementById('button-name2').name;
-    var quantidade = document.getElementById('quantidadesegundo').value;
-    var valor = document.getElementById('tamanhosegundo').value;
 
-    alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-    console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-}
+            var retornocarrinho = (quantidade+ "x " +nome)
+            document.getElementById('pedidos').innerHTML = retornocarrinho
+            console.log("Foi adicionado ao carrinho"+retornocarrinho)
 
-function adicionarcarrinho3(){
-    var nome = document.getElementById('button-name3').name;
-    var quantidade = document.getElementById('quantidadeterceiro').value;
-    var valor = document.getElementById('tamanhoterceiro').value;
-
-    alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-    console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-}
-
-function adicionarcarrinho4(){
-    var nome = document.getElementById('button-name4').name;
-    var quantidade = document.getElementById('quantidadequarto').value;
-    var valor = document.getElementById('tamanhoquarto').value;
-
-    alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-    console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
-}
-
-function precototalquantidade1(){
-    //1 lanche
-    var tamanhol = document.getElementById('tamanhoprimeiro').value;
-    var quantidade1 = document.getElementById('quantidadeprimeiro').value;
+            var valortotal = document.getElementById('totalprimeiro').value
+            document.getElementById('valortotal').innerHTML = ("TOTAL:"+valortotal+" R$")
+            break;
     
-    var total1 = tamanhol * quantidade1;
+        case 'gordodieta':
+            console.log("Entrou no Gordodieta")
+            var nome = document.getElementById('gordodieta').name;
+            var quantidade = document.getElementById('quantidadesegundo').value;
+            var valor = document.getElementById('tamanhosegundo').value;
 
-    document.getElementById('totalprimeiro').value = total1.toFixed(2);
-    console.log(total1)
+            alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+            console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+            break;
 
+        case 'gorduchao':
+            console.log("Entrou no gorduchao")
+            var nome = document.getElementById('gorduchao').name;
+            var quantidade = document.getElementById('quantidadeterceiro').value;
+            var valor = document.getElementById('tamanhoterceiro').value;
+        
+            alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+            console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+        break;
+
+        case 'dodia':
+            console.log("Entrou no dodia")
+            var nome = document.getElementById('dodia').name;
+            var quantidade = document.getElementById('quantidadequarto').value;
+            var valor = document.getElementById('tamanhoquarto').value;
+        
+            alert("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+            console.log("Você adicionou: " +quantidade+ "x " +nome+ " - " +valor+ "R$")
+        break;
+    }
+    
 }
 
-function precototalquantidade2(){
-    //2 lanche
-    var tamanho2 = document.getElementById('tamanhosegundo').value;
-    var quantidade2 = document.getElementById('quantidadesegundo').value;
+function somarvalor(name){
+    console.log(name)
+    switch (name) {
+        case 'onelanche':
+            //1 lanche
+            var tamanhol = document.getElementById('tamanhoprimeiro').value;
+            var quantidade1 = document.getElementById('quantidadeprimeiro').value;
+            
+            var total1 = tamanhol * quantidade1;
 
-    var total2 = tamanho2 * quantidade2;
+            document.getElementById('totalprimeiro').value = total1.toFixed(2);
+            console.log(total1)
+            break;
+    
+        case 'twolanche':
+            //2 lanche
+            var tamanho2 = document.getElementById('tamanhosegundo').value;
+            var quantidade2 = document.getElementById('quantidadesegundo').value;
 
-    document.getElementById('totalsegundo').value = total2.toFixed(2);
-    console.log(total2)
-}
+            var total2 = tamanho2 * quantidade2;
 
-function precototalquantidade3(){
-    //3 lanche
-    var valortamanho3 = document.getElementById('tamanhoterceiro').value;
-    var quantidade3 = document.getElementById('quantidadeterceiro').value;
+            document.getElementById('totalsegundo').value = total2.toFixed(2);
+            console.log(total2)
+            break;
 
-    var total3 = valortamanho3 * quantidade3;
+        case 'trilanche':
+            //3 lanche
+            var valortamanho3 = document.getElementById('tamanhoterceiro').value;
+            var quantidade3 = document.getElementById('quantidadeterceiro').value;
 
-    document.getElementById('totalterceiro').value = total3.toFixed(2);
-    console.log(total3)
-}
+            var total3 = valortamanho3 * quantidade3;
 
-function precototalquantidade4(){
-    //4 lanche
-    var tamanho4 = document.getElementById('tamanhoquarto').value;
-    var quantidade4 = document.getElementById('quantidadequarto').value;
+            document.getElementById('totalterceiro').value = total3.toFixed(2);
+            console.log(total3)
+            break;
 
-    var total4 = tamanho4 * quantidade4;
+        case 'fourlanche':
+            //4 lanche
+            var tamanho4 = document.getElementById('tamanhoquarto').value;
+            var quantidade4 = document.getElementById('quantidadequarto').value;
 
-    document.getElementById('totalquarto').value = total4.toFixed(2);
-    console.log(total4)
+            var total4 = tamanho4 * quantidade4;
+
+            document.getElementById('totalquarto').value = total4.toFixed(2);
+            console.log(total4)
+            break;
+
+    }
 }
