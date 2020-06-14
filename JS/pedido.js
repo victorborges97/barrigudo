6bce2612a341fd91f132f9c21128e97dd00fadd9
João Victor Dutra de Oliveria Borges <borges.jvdo@gmail.com>
2020-06-14 13:49:17 -0300

    correções

--- JS/pedido.js
+++ JS/pedido.js
@@ -6,7 +6,7 @@ var pedido4 = [0,0,0];
 var total = 0;
 
 function Reiniciar(){
-    total = 0
+    total = 0;
     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2)); //Atualiza o Valor total
 
     document.getElementById("00").innerText = (""); 
@@ -45,40 +45,40 @@ function adicionarcarrinho(id){
             var quantidade1 = parseInt(document.getElementById('quantidadeprimeiro').value);
             var valor1 = parseFloat(document.getElementById('tamanhoprimeiro').value);
 
-            if (quantidade1 > 0 & valor1 > 0) {
+            if (quantidade1 > 0 && valor1 > 0) {
 
-                alert("Você adicionou: " +quantidade1+ "x " +nome+ " - " +valor1+ "R$")
+                alert("Você adicionou: " +quantidade1+ "x " +nome+ " - " +valor1+ "R$");
 
                 if (valor1==14.90){
-                    var preço = valor1 * quantidade1;
-                    total = preço + total;
+                    var preco = valor1 * quantidade1;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2)); //Atualiza o Valor total
                     pedido1 [0] = quantidade1 + pedido1[0];
-                    document.getElementById("00").innerText = (pedido1 [0]+ "X " +nome+ " Simples") //Atualiza a quantidade do pedido
+                    document.getElementById("00").innerText = (pedido1 [0]+ "X " +nome+ " Simples"); //Atualiza a quantidade do pedido
                     
                 }
 
                 else if (valor1==17.00){
-                    var preço = valor1 * quantidade1;
-                    total = preço + total;
+                    var preco = valor1 * quantidade1;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido1 [1] = quantidade1 + pedido1[1];
-                    document.getElementById("01").innerText = (pedido1 [1]+ "X " +nome+ " Duplo") //Atualiza a quantidade do pedido
+                    document.getElementById("01").innerText = (pedido1 [1]+ "X " +nome+ " Duplo"); //Atualiza a quantidade do pedido
                     
                 }
 
                 else if (valor1==23.00){
-                    var preço = valor1 * quantidade1;
-                    total = preço + total;  
+                    var preco = valor1 * quantidade1;
+                    total = preco + total;  
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido1 [2] = quantidade1 + pedido1[2];
-                    document.getElementById("02").innerText = (pedido1 [2]+ "X " +nome+ " Triplo") //Atualiza a quantidade do pedido
+                    document.getElementById("02").innerText = (pedido1 [2]+ "X " +nome+ " Triplo"); //Atualiza a quantidade do pedido
                     
                 }
-                reset()
+                reset();
             }
             else{
-                alert("Você não selecionou a quantidade maior que zero ou tamanho do lache")
+                alert("Você não selecionou a quantidade maior que zero ou tamanho do lache");
                 break;
             }
 
@@ -91,37 +91,37 @@ function adicionarcarrinho(id){
             var quantidade2 = parseInt(document.getElementById('quantidadesegundo').value);
             var valor2 = parseFloat(document.getElementById('tamanhosegundo').value);
 
-           if (quantidade2 > 0 & valor2 > 0) {
+           if (quantidade2 > 0 && valor2 > 0) {
 
-                alert("Você adicionou: " +quantidade2+ "x " +nome+ " - " +valor2+ "R$")
+                alert("Você adicionou: " +quantidade2+ "x " +nome+ " - " +valor2+ "R$");
 
                 if (valor2==14.90){
-                    var preço = valor2 * quantidade2;
-                    total = preço + total;
+                    var preco = valor2 * quantidade2;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2)); //Atualiza o Valor total
                     pedido2 [0] = quantidade2 + pedido2[0];
-                    document.getElementById("10").innerText = (pedido2 [0]+ "X " +nome+ " Simples") //Atualiza a quantidade do pedido
+                    document.getElementById("10").innerText = (pedido2 [0]+ "X " +nome+ " Simples"); //Atualiza a quantidade do pedido
                 }
 
                 else if (valor2==17.00){
-                    var preço = valor2 * quantidade2;
-                    total = preço + total;
+                    var preco = valor2 * quantidade2;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido2 [1] = quantidade2 + pedido2[1];
-                    document.getElementById("11").innerText = (pedido2 [1]+ "X " +nome+ " Duplo") //Atualiza a quantidade do pedido
+                    document.getElementById("11").innerText = (pedido2 [1]+ "X " +nome+ " Duplo"); //Atualiza a quantidade do pedido
                 }
 
                 else if (valor2==23.00){
-                    var preço = valor2 * quantidade2;
-                    total = preço + total;  
+                    var preco = valor2 * quantidade2;
+                    total = preco + total;  
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido2 [2] = quantidade2 + pedido2[2];
-                    document.getElementById("12").innerText = (pedido2 [2]+ "X " +nome+ " Triplo") //Atualiza a quantidade do pedido
+                    document.getElementById("12").innerText = (pedido2 [2]+ "X " +nome+ " Triplo"); //Atualiza a quantidade do pedido
                 }
-                reset()
+                reset();
             }
             else{
-                alert("Você não selecionou a quantidade maior que zero")
+                alert("Você não selecionou a quantidade maior que zero");
                 break;
             }
             break;
@@ -133,29 +133,29 @@ function adicionarcarrinho(id){
             var quantidade3 = parseInt(document.getElementById('quantidadeterceiro').value);
             var valor3 = parseFloat(document.getElementById('tamanhoterceiro').value);
 
-           if (quantidade3 > 0 & valor3 > 0) {
+           if (quantidade3 > 0 && valor3 > 0) {
 
-                alert("Você adicionou: " +quantidade3+ "x " +nome+ " - " +valor3+ "R$")
+                alert("Você adicionou: " +quantidade3+ "x " +nome+ " - " +valor3+ "R$");
 
                 if (valor3==14.90){
-                    var preço = valor3 * quantidade3;
-                    total = preço + total;
+                    var preco = valor3 * quantidade3;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2)); //Atualiza o Valor total
                     pedido3 [0] = quantidade3 + pedido3[0];
                     document.getElementById("20").innerText = (pedido3 [0]+ "X " +nome+ " Simples") //Atualiza a quantidade do pedido
                 }
 
                 else if (valor3==17.00){
-                    var preço = valor3 * quantidade3;
-                    total = preço + total;
+                    var preco = valor3 * quantidade3;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido3 [1] = quantidade3 + pedido3[1];
                     document.getElementById("21").innerText = (pedido3 [1]+ "X " +nome+ " Duplo") //Atualiza a quantidade do pedido
                 }
 
                 else if (valor3==23.00){
-                    var preço = valor3 * quantidade3;
-                    total = preço + total;  
+                    var preco = valor3 * quantidade3;
+                    total = preco + total;  
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido3 [2] = quantidade3 + pedido3[2];
                     document.getElementById("22").innerText = (pedido3 [2]+ "X " +nome+ " Triplo") //Atualiza a quantidade do pedido
@@ -173,29 +173,29 @@ function adicionarcarrinho(id){
             var quantidade4 = parseInt(document.getElementById('quantidadequarto').value);
             var valor4 = parseFloat(document.getElementById('tamanhoquarto').value);
 
-            if (quantidade4 > 0 & valor4 > 0) {
+            if (quantidade4 > 0 && valor4 > 0) {
 
                 alert("Você adicionou: " +quantidade4+ "x " +nome+ " - " +valor4+ "R$")
 
                 if (valor4==14.90){
-                    var preço = valor4 * quantidade4;
-                    total = preço + total;
+                    var preco = valor4 * quantidade4;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2)); //Atualiza o Valor total
                     pedido4 [0] = quantidade4 + pedido4[0];
                     document.getElementById("30").innerText = (pedido4 [0]+ "X " +nome+ " Simples") //Atualiza a quantidade do pedido
                 }
 
                 else if (valor4==17.00){
-                    var preço = valor4 * quantidade4;
-                    total = preço + total;
+                    var preco = valor4 * quantidade4;
+                    total = preco + total;
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido4 [1] = quantidade4 + pedido4[1];
                     document.getElementById("31").innerText = (pedido4 [1]+ "X " +nome+ " Duplo") //Atualiza a quantidade do pedido
                 }
 
                 else if (valor4==23.00){
-                    var preço = valor4 * quantidade4;
-                    total = preço + total;  
+                    var preco = valor4 * quantidade4;
+                    total = preco + total;  
                     document.getElementById("valortotal").innerText = ("TOTAL: " + total.toFixed(2));
                     pedido4 [2] = quantidade4 + pedido4[2];
                     document.getElementById("32").innerText = (pedido4 [2]+ "X " +nome+ " Triplo") //Atualiza a quantidade do pedido
